641dbca902bbc5f5109b536a72bdf627b04bdce7
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-30 13:49:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     language = ""
-     language << programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
