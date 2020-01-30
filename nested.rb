3070a5f4b84003d7e0c9101b5b654204354d6cee
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-30 15:03:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = :known_for
-    programmer_hash[:yukihiro_matsumoto][:known_for => "Ruby"]
-    
+programmer_hash = { :yukihiro_matsumoto => {
+      :known_for =>"Ruby",
+      :languages => ["LISP", "C"] }
+    }
 end
 
 def changing_alan
